1fc44d3a5387b51d29c03cb977e50292698c1cc3
Karlaleticiaavila <Kbliaass@gmail.com>
2026-02-06 15:30:38 -0800

    Exercise 1 typescript oop and ts

--- src/ex05.ts
+++ src/ex05.ts
@@ -1,10 +1,18 @@
 // 5. Create a function named `countVowels` that takes a string as a parameter.
 //    The function should return the number of vowels (a, e, i, o, u) in the string.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
+function countVowels(word: string): number {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
 
-function countVowels(word) {
-
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.indexOf(word[i]) !== -1) {
+      count++;
+    }
+  }
+  return count;
 }
+
 
 // Expected output:
 console.log(countVowels("hello"));    // 2
